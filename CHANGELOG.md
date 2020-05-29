9494a8db8bc85e7e962c82de1d33de9696937e77
Ken Innes <primehalo@gmail.com>
2020-05-29 15:23:25 -0700

    Updated Russian Translations

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
+## [1.0.1] - 2020-05-29
+### Added
+- Russian translations for previously untranslated text
+
+### Changed
+- Updated some Russian translations
+
 ## [1.0.0] - 2020-03-27
 ### Changed
 - Changed the hardcoded ellipse that is appended to truncated message to instead use the ellipse language string
---
+++ language/ru/info_acp_primenotify.php
@@ -0,0 +1,59 @@
+<?php
+/**
+*
+* Prime Notify extension for the phpBB Forum Software package.
+*
+* @copyright (c) 2018 Ken F. Innes IV <https://www.absoluteanime.com>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*
+*/
+
+/**
+* DO NOT CHANGE
+*/
+if (!defined('IN_PHPBB'))
+{
+	exit;
+}
+
+if (empty($lang) || !is_array($lang))
+{
+	$lang = array();
+}
+
+// DEVELOPERS PLEASE NOTE
+//
+// All language files should use UTF-8 as their encoding and the files must not contain a BOM.
+//
+// Placeholders can now contain order information, e.g. instead of
+// 'Page %s of %s' you can (and should) write 'Page %1$s of %2$s', this allows
+// translators to re-order the output of data while ensuring it remains correct
+//
+// You do not need this where single placeholders are used, e.g. 'Message %d' is fine
+// equally where a string contains only two placeholders which are used to wrap text
+// in a url you again do not need to specify an order e.g., 'Click %sHERE%s' is fine
+//
+// Some characters you may want to copy&paste:
+// ’ » “ ” …
+//
+
+$lang = array_merge($lang, array(
+	'ACP_PRIMENOTIFY_TITLE'					=> 'Prime уведомления',
+	'ACP_PRIMENOTIFY_SETTINGS'				=> 'Настройки уведомлений',
+	'ACP_PRIMENOTIFY_BASIC_SETTINGS'		=> 'Основные настройки',
+	'ACP_PRIMENOTIFY_SETTINGS_SAVED'		=> 'Настройки Prime уведомлений успешно обновлены!',
+	'ACP_PRIMENOTIFY_SETTINGS_LOG'			=> '<strong>Изменены настройки Prime уведомлений</strong>',
+	'ACP_PRIMENOTIFY_USER_CHOICE'			=> 'Выбор пользователя',
+
+	// Settings
+	'ACP_PRIMENOTIFY_ENABLE_POST'			=> 'Разрешить для сообщений',
+	'ACP_PRIMENOTIFY_ENABLE_POST_EXPLAIN'	=> 'Включать текст сообщения в email-уведомление.',
+	'ACP_PRIMENOTIFY_ENABLE_PM'				=> 'Разрешить для личных сообщений',
+	'ACP_PRIMENOTIFY_ENABLE_PM_EXPLAIN'		=> 'Включать текст личного сообщения в email-уведомление.',
+	'ACP_PRIMENOTIFY_KEEP_BBCODES'			=> 'Сохранять BBCode',
+	'ACP_PRIMENOTIFY_KEEP_BBCODES_EXPLAIN'	=> 'Email-сообщения отправляются как простой текст, и BBCode не могут их отформатировать. Сохраненный BBCode поможет понять оригинальное форматирование сообщения, удаленный облегчит чтение сообщения.',
+	'ACP_PRIMENOTIFY_ALWAYS_SEND'			=> 'Email-уведомление для каждого нового сообщения',
+	'ACP_PRIMENOTIFY_ALWAYS_SEND_EXPLAIN'	=> 'Без данной настройки email-уведомление отправляется только для первого сообщения с момента последнего посещения пользователем темы.',
+	'ACP_PRIMENOTIFY_TRUNCATE'				=> 'Максимальное количество символов в сообщениях',
+	'ACP_PRIMENOTIFY_TRUNCATE_EXPLAIN'		=> 'Укорачивает сообщения до указанной длины. Введите 0 для снятия ограничений.',
+));
